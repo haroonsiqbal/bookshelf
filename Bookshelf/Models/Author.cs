f22bb76c2948b78065659d88b9468e414dd6b45a
Haroon Iqbal <haroonsiqbal@gmail.com>
2019-11-22 15:08:56 -0600

    set up

---
+++ Bookshelf/Models/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelf.Models
+{
+    public class Author
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get; set; }
+
+        public string Penname { get; set; }
+        public string PreferredGenre { get; set; }
+        public List<Book> Books { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Required]
+        [MaxLength(13)]
+        [MinLength(10)]
+        public int ISBN { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public DateTime PublishDate { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public ApplicationUser Owner { get; set; } 
+
+    }
+}
